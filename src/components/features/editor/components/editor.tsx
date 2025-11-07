c3a70af5c18eb88a1f7d9a3fbb972b14216f803b
Mecil.Meng <mecil9@gmail.com>
2025-11-07 17:26:59 +0800

    15: editor setup

--- src/components/features/editor/components/editor.tsx
+++ src/components/features/editor/components/editor.tsx
@@ -2,7 +2,7 @@
  * @Author: Mecil Meng
  * @Date: 2025-11-07 12:47:53
  * @LastEditors: Mecil Meng
- * @LastEditTime: 2025-11-07 12:50:19
+ * @LastEditTime: 2025-11-07 16:54:19
  * @FilePath: /nodebase/src/components/features/editor/components/editor.tsx
  * @Description:
  *
@@ -10,8 +10,28 @@
  */
 "use client";
 
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type Node,
+  type Edge,
+  type NodeChange,
+  type EdgeChange,
+  type Connection,
+  Background,
+  Controls,
+  MiniMap,
+  Panel,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "../../workflows/hooks/use-workflows";
+import { nodeComponents } from "@/config/node-components";
+import { AddNodeButton } from "./add-node-button";
 
 export const EditorLoading = () => {
   return <LoadingView message={"Loading workflow..."} />;
@@ -21,7 +41,56 @@ export const EditorError = () => {
   return <ErrorView message={"Error loading workflow"} />;
 };
 
+const initialNodes = [
+  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
+  { id: "n2", position: { x: 0, y: 100 }, data: { label: "Node 2" } },
+];
+
+const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
+
 export const Editor = ({ workflowId }: { workflowId: string }) => {
   const { data: workflow } = useSuspenseWorkflow(workflowId);
-  return <div>{JSON.stringify(workflow, null, 2)}</div>;
+
+  const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+  const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
+  );
+  const onConnect = useCallback(
+    (params: Connection) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    []
+  );
+
+  return (
+    <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        nodeTypes={nodeComponents}
+        proOptions={{
+          hideAttribution: true,
+        }}
+      >
+        <Background />
+        <Controls />
+        <MiniMap />
+        <Panel position="top-right">
+          <AddNodeButton />
+        </Panel>
+      </ReactFlow>
+    </div>
+  );
 };
--- src/components/features/workflows/server/routers.ts
+++ src/components/features/workflows/server/routers.ts
@@ -2,7 +2,7 @@
  * @Author: Mecil Meng
  * @Date: 2025-11-05 11:51:16
  * @LastEditors: Mecil Meng
- * @LastEditTime: 2025-11-07 13:10:06
+ * @LastEditTime: 2025-11-07 16:23:22
  * @FilePath: /nodebase/src/components/features/workflows/server/routers.ts
  * @Description:
  *
@@ -10,6 +10,7 @@
  */
 import { generateSlug } from "random-word-slugs";
 import prisma from "@/lib/db";
+import type { Node, Edge } from "@xyflow/react";
 import {
   createTRPCRouter,
   premiumProcedure,
@@ -17,6 +18,7 @@ import {
 } from "@/trpc/init";
 import z from "zod";
 import { PAGINATION } from "@/config/constants";
+import { NodeType } from "@/generated/prisma/enums";
 
 export const workflowsRouter = createTRPCRouter({
   create: premiumProcedure.mutation(({ ctx }) => {
@@ -24,6 +26,13 @@ export const workflowsRouter = createTRPCRouter({
       data: {
         name: generateSlug(3),
         userId: ctx.auth.user.id,
+        nodes: {
+          create: {
+            type: NodeType.INITIAL,
+            position: { x: 0, y: 0 },
+            name: NodeType.INITIAL,
+          },
+        },
       },
     });
   }),
@@ -53,12 +62,37 @@ export const workflowsRouter = createTRPCRouter({
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
-      return await prisma.workflow.findUniqueOrThrow({
+      const workflow = await prisma.workflow.findUniqueOrThrow({
         where: {
           id: input.id,
           userId: ctx.auth.user.id,
         },
+        include: { nodes: true, connections: true },
       });
+
+      // Transform server nodes to react-flow
+      const nodes: Node[] = workflow.nodes.map((node) => ({
+        id: node.id,
+        type: node.type,
+        position: node.position as { x: number; y: number },
+        data: (node.data as Record<string, unknown>) || {},
+      }));
+
+      // Transform server components to react-flow compatible edges
+      const edges: Edge[] = workflow.connections.map((connection) => ({
+        id: connection.id,
+        source: connection.fromNodeId,
+        target: connection.toNodeId,
+        sourceHandle: connection.fromOutput,
+        targetHandle: connection.toInput,
+      }));
+
+      return {
+        id: workflow.id,
+        name: workflow.name,
+        nodes,
+        edges,
+      };
     }),
   getMany: protectedProcedure
     .input(
